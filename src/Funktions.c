7574f25be35479cb4f81e5eaddf472d03aefca2a
sebub <105357150+sebub@users.noreply.github.com>
2022-10-11 16:56:30 +0200

    s

---
+++ src/Funktions.c
@@ -0,0 +1,5 @@
+//
+// Created by sebbe on 09/10/2022.
+//
+
+#include "Funktions.h"
---
+++ src/Funktions.h
@@ -0,0 +1,8 @@
+//
+// Created by sebbe on 09/10/2022.
+//
+
+#ifndef PROJECT1_FUNKTIONS_H
+#define PROJECT1_FUNKTIONS_H
+
+#endif //PROJECT1_FUNKTIONS_H
